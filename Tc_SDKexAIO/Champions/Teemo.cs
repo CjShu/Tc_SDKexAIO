43475a4801c40f3d2a2f5ed2cd5c27ae064a70b6
CjShu <az937182@gmail.com>
2016-08-03 14:58:17 +0800

    + Teemo

--- Tc_SDKexAIO/Champions/Teemo.cs
+++ Tc_SDKexAIO/Champions/Teemo.cs
@@ -35,6 +35,12 @@
         private static Spell Q, W, E, R;
         private static Vector3 LastQ;
         private static bool CastQQQ = false;
+        private static float LasQ = Variables.TickCount;
+        private static Vector3 Position;
+        private static bool ShotR => Player.HasBuff("ToxicShot");
+        private static int RCast;
+        private static float RRange => 300 * R.Level;
+        private static bool Trap => GameObjects.Get<Obj_AI_Base>().Where(x => x.Name == "Noxious Trap").Any(x => Position.Distance(x.Position) <= 250);
 
         internal static void Init()
         {
@@ -49,8 +55,9 @@
             {
                 QMenu.GetBool("ComboQ", "Comno Q");
                 QMenu.GetBool("HarassQ", "Harass Q");
-                QMenu.GetBool("LaneClearQ", "LaneClear Q");
+                QMenu.GetBool("ADQ", "Use Q AD");
                 QMenu.GetBool("KSQ", "KillStel Q");
+                QMenu.GetBool("CheckAA", "Check AA", false);
             }
 
             var WMenu = Menu.Add(new Menu("W", "W.Set | W 設定"));
@@ -65,13 +72,12 @@
                 RMenu.GetBool("Charge", "Charges of R before using R :)", false);
                 RMenu.Add(new MenuKeyBind("AutoR", "Auto R Key", Keys.T, KeyBindType.Toggle));
                 RMenu.GetSlider("RCount", "R Count >=", 1, 1, 5);
+                RMenu.GetBool("Gapcloser", "Gapcloser R");
             }
 
             var DrawMenu = Menu.Add(new Menu("Draw", "Draw"));
             {
                 DrawMenu.GetBool("Q", "Q Range");
-                DrawMenu.GetBool("W", "W Range");
-                DrawMenu.GetBool("E", "E Range");
                 DrawMenu.GetBool("R", "R Range");
                 DrawMenu.GetBool("DrawDamge", "Draw Damge Ks", false);
             }
@@ -79,21 +85,123 @@
             Game.OnUpdate += OnUpdate;
             Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast;
             Events.OnGapCloser += OnGapCloser;
+            Drawing.OnDraw += OnDraw;
+            Variables.Orbwalker.OnAction += OnAction;
         }
 
-        private static void OnGapCloser(object sender, Events.GapCloserEventArgs e)
+        private static void OnAction(object sender, OrbwalkingActionArgs args)
+        {
+            try
+            {
+                var ComboQ = Menu["Q"]["ComboQ"].GetValue<MenuBool>().Value;
+                var HarassQ = Menu["Q"]["HarassQ"].GetValue<MenuBool>().Value;
+                var ADQ = Menu["Q"]["ADQ"].GetValue<MenuBool>();
+                var CheckQA = Menu["Q"]["CheckAA"].GetValue<MenuBool>();
+
+                if (args.Type == OrbwalkingType.AfterAttack)
+                {
+                    var QTarget = GetTarget(680, Q.DamageType);
+
+                    var Attack = GetAttackRange(QTarget);
+
+                    if (QTarget != null && Combo && ComboQ)
+                    {
+                        if (CheckQA)
+                        {
+                            if (ADQ && Marksman.Contains(QTarget.CharData.BaseSkinName) && Q.IsReady() && Q.IsInRange(QTarget, -170))
+                            {
+                                Q.Cast(QTarget);
+                            }
+                            else if (Q.IsReady() && Q.IsInRange(QTarget, -100))
+                            {
+                                Q.Cast(QTarget);
+                            }
+                        }
+                        else if (ADQ && Marksman.Contains(QTarget.CharData.BaseSkinName) && Q.IsReady() && Q.IsInRange(QTarget))
+                        {
+                            Q.Cast(QTarget);
+                        }
+                        else if (ADQ && Q.IsReady() && Q.IsInRange(QTarget))
+                        {
+                            Q.Cast(QTarget);
+                        }
+                    }
+                    if (QTarget != null && Harass && HarassQ)
+                    {
+                        if (CheckQA)
+                        {
+                            if (Q.IsReady() && Q.IsInRange(QTarget, -70))
+                            {
+                                Q.Cast(QTarget);
+                            }
+                        }
+                        else if (Q.IsReady() && Q.IsReady() && Q.IsInRange(QTarget))
+                        {
+                            Q.Cast(QTarget);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error In OnAction" + ex);
+            }
+        }
+
+        private static void OnDraw(EventArgs args)
         {
             throw new NotImplementedException();
+        }
+
+        private static void OnGapCloser(object sender, Events.GapCloserEventArgs args)
+        {
+            if (!R.IsReady()) return;
+                
+            if (Menu["R"]["Gapcloser"].GetValue<MenuBool>())
+            {
+                if (args.Sender.IsValidTarget() && args.Sender.IsFacing(Player) && args.Sender.IsTargetable)
+                {
+                    R.Cast(args.Sender.Position);
+                }
+            }
         }
 
         private static void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            throw new NotImplementedException();
+            if (sender.IsMe && args.SData.Name == "TeemoRCast")
+            {
+                RCast = Variables.TickCount;
+            }
         }
 
         private static void OnUpdate(EventArgs args)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                R = new Spell(SpellSlot.R, RRange);
+
+                if (Player.IsDead)
+                    return;
+
+                //ComboLogic(args);
+
+                //LaneClear(args);
+
+                //JungleClear(args);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error In OnUpdate" + ex);
+            }
         }
+
+        private static string[] Marksman =
+        {
+            "Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Jinx", "Kalista",
+            "KogMaw", "Lucian", "MissFortune", "Quinn", "Sivir", "Teemo", "Tristana", "Twitch", "Urgot", "Varus",
+            "Vayne"
+        };
     }
 }
